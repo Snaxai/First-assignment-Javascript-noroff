b2a1e26f2b835f460ea2be594c03c4249fdc5e5b
Daniel Mossestad <daniem15@uia.no>
2022-01-12 18:48:22 +0100

    start of adding JS

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -1,1 +1,8 @@
-// Javascript goes here
+const salaryElement = document.getElementById("salary");
+const laptopsElement = document.getElementById("laptops");
+const bankButtonElement = document.getElementById("bankButton");
+const loanButtonElement = document.getElementById("loanButton");
+const workButtonElement = document.getElementById("workButton");
+
+let laptops = [];
+let salary = 0;
