39e574c2747d39db0cd8538e9074749056c26d3b
Daniel Mossestad <daniem15@uia.no>
2022-01-14 09:58:50 +0100

    added add-salary and depositToBank function

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -1,8 +1,36 @@
-const salaryElement = document.getElementById("salary");
-const laptopsElement = document.getElementById("laptops");
-const bankButtonElement = document.getElementById("bankButton");
-const loanButtonElement = document.getElementById("loanButton");
-const workButtonElement = document.getElementById("workButton");
+const salaryEl = document.getElementById("salary");
+const moneyInBankEl = document.getElementById("moneyInBank");
+const laptopsEl = document.getElementById("laptops");
+const bankButtonEl = document.getElementById("bankButton");
+const loanButtonEl = document.getElementById("loanButton");
+const workButtonEl = document.getElementById("workButton");
 
 let laptops = [];
 let salary = 0;
+let bank = 0;
+
+const doWork = () => {
+  salary += 500;
+  console.log("Work Work and salary is now: " + salary);
+  updateSalary();
+};
+
+const updateSalary = () => {
+  salaryEl.innerHTML = salary;
+};
+
+const depositToBank = () => {
+  // need to do some calculations if there is a loan
+  bank += salary;
+  salary = 0;
+  console.log("salary moved to bank");
+  updateSalary();
+  updateBank();
+};
+
+const updateBank = () => {
+  moneyInBankEl.innerHTML = bank;
+};
+
+workButtonEl.addEventListener("click", doWork);
+bankButtonEl.addEventListener("click", depositToBank);
