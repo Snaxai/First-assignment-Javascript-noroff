76a0e1723dd0af125c5ae13e3929ba8dadf13268
Daniel Mossestad <daniem15@uia.no>
2022-01-17 16:54:03 +0100

    mvp functionality done

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -6,11 +6,43 @@ const bankButtonEl = document.getElementById("bankButton");
 const loanButtonEl = document.getElementById("loanButton");
 const workButtonEl = document.getElementById("workButton");
 const repayLoanEl = document.getElementById("repayLoan");
+const priceEl = document.getElementById("price");
+const descriptionEl = document.getElementById("description");
+const laptopImgEl = document.getElementById("laptopImg");
+const buyButton = document.getElementById("buyButton");
 
 let laptops = [];
+let selectedLaptop = {};
 let salary = 0;
 let bank = 0;
 let loan = 0;
+
+fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+  .then((response) => response.json())
+  .then((data) => (laptops = data))
+  .then((laptops) => addLaptopsToSelect(laptops));
+
+const addLaptopsToSelect = (laptops) => {
+  laptops.forEach((laptop) => addLaptopToSelect(laptop));
+  priceEl.innerText = laptops[0].price;
+  descriptionEl.innerText = laptops[0].description;
+  selectedLaptop = laptops[0];
+};
+
+const addLaptopToSelect = (laptop) => {
+  const laptopElement = document.createElement("option");
+  laptopElement.value = laptop.id;
+  laptopElement.appendChild(document.createTextNode(laptop.title));
+  laptopsEl.appendChild(laptopElement);
+};
+
+const handleLaptopChange = (e) => {
+  selectedLaptop = laptops[e.target.selectedIndex];
+  priceEl.innerText = selectedLaptop.price;
+  descriptionEl.innerText = selectedLaptop.description;
+  laptopImgEl.src =
+    "https://noroff-komputer-store-api.herokuapp.com/" + selectedLaptop.image;
+};
 
 const doWork = () => {
   salary += 100;
@@ -60,12 +92,14 @@ const loanPrompt = () => {
       "Please enter the amount you want to loan. You can maximum loan " +
         bank * 2
     );
-    let loan = parseInt(loanMessage);
-    requestLoan(loan);
+    let loanInput = parseInt(loanMessage);
+    requestLoan(loanInput);
 
     updateLoan();
     updateBank();
-    createRepayLoanButton();
+    if (loan > 0) {
+      createRepayLoanButton();
+    }
   } else {
     window.alert("Pay back your current loan before taking a new loan");
   }
@@ -75,9 +109,32 @@ const createRepayLoanButton = () => {
   let repayBtn = document.createElement("button");
   repayBtn.innerHTML = "Repay Loan";
 
-  repayBtn.onclick = () => alert("Button is clicked. Mabye loan repayed?");
+  repayBtn.onclick = () => repayLoan();
 
   repayLoanEl.append(repayBtn);
+};
+
+const removeRepayLoanButton = () => {};
+
+const repayLoan = () => {
+  let loanToBePayed = loan;
+  let currentSalary = salary;
+
+  if (currentSalary - loan < 0) {
+    loan = loan - salary;
+    salary = 0;
+  } else if (loanToBePayed - currentSalary < 0) {
+    salary = salary - loan;
+    loan = 0;
+  } else {
+    loan = loan - salary;
+    salary = loanToBePayed - currentSalary;
+  }
+
+  updateLoan();
+  updateSalaryHTML();
+
+  console.log("Repayed loan");
 };
 
 const requestLoan = (loanAmount) => {
@@ -86,13 +143,29 @@ const requestLoan = (loanAmount) => {
     loan += loanAmount;
 
     console.log("You entered: " + loanAmount);
+  } else if (bank <= 0) {
+    alert(
+      "You dont have any money and therefore can not make a loan. Go work first"
+    );
   } else {
-    console.log("No number entered or you can not loan that much");
+    alert("Not a valid value");
   }
 
   return loanAmount;
 };
 
+const handleBuyLaptop = () => {
+  if (bank < selectedLaptop.price) {
+    alert("Sufficient funds");
+  } else {
+    bank -= selectedLaptop.price;
+    updateBank();
+    alert(`Congratulations! You now own a ${selectedLaptop.title}!`);
+  }
+};
+
 workButtonEl.addEventListener("click", doWork);
 bankButtonEl.addEventListener("click", depositToBank);
 loanButtonEl.addEventListener("click", loanPrompt);
+laptopsEl.addEventListener("change", handleLaptopChange);
+buyButton.addEventListener("click", handleBuyLaptop);
